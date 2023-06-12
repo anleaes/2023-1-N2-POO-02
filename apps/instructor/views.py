393d8de991cf4a7d6ae129a7680349dfaf87a0ae
CarlosKSL <carlosluan.ksl@gmail.com>
2023-06-12 19:05:32 -0300

    finalizando views

--- apps/instructor/views.py
+++ apps/instructor/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Instructor
+from rest_framework import viewsets
+from .serializer import InstructorSerializer
 
 # Create your views here.
+
+class InstructorViewSet(viewsets.ModelViewSet):
+    queryset = Instructor.objects.all()
+    serializer_class = InstructorSerializer  
