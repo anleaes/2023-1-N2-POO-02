9af2bd02011d2fd4aa4758af29204e301f0dcee8
CarlosKSL <carlosluan.ksl@gmail.com>
2023-06-12 18:40:05 -0300

    adicionando verbose_name instructor

--- apps/instructor/apps.py
+++ apps/instructor/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class InstructorConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'instructor'
+    verbose_name = 'Instructor'
