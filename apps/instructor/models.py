922616b9c0808084394a9f8789d40bb0e33b77a2
CarlosKSL <carlosluan.ksl@gmail.com>
2023-06-12 18:52:30 -0300

    criada a função que retorna especialidade

--- apps/instructor/models.py
+++ apps/instructor/models.py
@@ -7,3 +7,6 @@ from django.contrib.auth.models import User
 class Instructor(models.Model):
     especialidade = models.CharField('Nome', max_length=50)
     user = models.ForeignKey(User, on_delete=models.CASCADE) 
+    
+    def __str__(self):
+        return self.especialidade
