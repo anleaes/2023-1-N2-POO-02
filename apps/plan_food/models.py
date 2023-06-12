444d1676819c44719ae41cbb8946cfeb1c3c61d9
JoaoAndre78 <dorinho1978@gmail.com>
2023-06-11 21:24:20 -0300

    Criação

--- apps/plan_food/models.py
+++ apps/plan_food/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Food_Plan(models.Model):
+    alimento = models.CharField('alimento', max_length=200)
+    quantidade = models.TextField('quantidade', max_length=15)
+    
+    class Meta:
+        verbose_name = 'Plan_food'
+        verbose_name_plural = 'Plan_foods'
+        ordering =['id']
+
+    def __str__(self):
+        return self.name
