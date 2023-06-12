156a9e9b8148151d843b3bda5eb01c4cb9dfe46c
CarlosKSL <carlosluan.ksl@gmail.com>
2023-06-12 00:11:56 -0300

    adicionado o caminho na urls

--- gymapp/urls.py
+++ gymapp/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('exercises/', include('exercises.urls', namespace='exercises')),
     path('plan_food/', include('plan_food.urls', namespace='plan_food')),
     path('gym', include('gym.urls', namespace='gym')),
+    path('students/', include('student.urls', namespace='student')),
 ]
