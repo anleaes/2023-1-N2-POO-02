f6bacc0d3012becd6d739c64b48135d36c239467
CarlosKSL <carlosluan.ksl@gmail.com>
2023-06-12 00:28:38 -0300

    executando makemigration

--- apps/student/models.py
+++ apps/student/models.py
@@ -3,13 +3,13 @@ from django.db import models
 # Create your models here.
 
 #from training.models import Training
-from food_plan.models import Food_Plan
+from plan_food.models import Plan_food
 from django.contrib.auth.models import User
 
 class Student(models.Model):
     objetivo = models.CharField('objetivo', max_length=100)
    # training = models.ForeignKey(Training, on_delete=models.CASCADE)
-    food_plan = models.ForeignKey(Food_Plan, on_delete=models.CASCADE)
+    food_plan = models.ForeignKey(Plan_food, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE) 
 
     def __str__(self):
